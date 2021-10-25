9254305458bbebd707079120f6bd6624153b8ebf
Antti Ruuskanen <antti.ruuskanen@hotmail.com>
2021-10-25 12:49:44 +0300

    Flask with base.html and testing

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def test():
-    return "Hello world!"
+def welcome():
+    return render_template("index.html", title="Welcome")
+
+@app.route("/test1")
+def test1():
+    return render_template("test1.html", title="Testing")
+
+@app.route("/login", methods=["POST"])
+def login():
+    1+1
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
