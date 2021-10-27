57cf7c9f6b4195b668b93dfb27f468b1181b556d
Antti Ruuskanen <antti.ruuskanen@hotmail.com>
2021-10-27 10:52:37 +0300

    Skeletor is working, but from now on better structure is needed + DB

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session
 
 app = Flask(__name__)
+
 
 logged = False
 print(logged)
