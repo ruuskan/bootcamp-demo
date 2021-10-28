efc2a2a6e4a5783d1277157fc24e313b8b892bb7
Antti Ruuskanen <antti.ruuskanen@hotmail.com>
2021-10-28 11:57:01 +0300

    Date picker modified
    	-Date picker functionality tested and made better

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request, flash, session
 import datetime
 import requests
+from werkzeug.utils import redirect
 import data_from_veikkaus as dfv
 app = Flask(__name__)
+defdate1 = datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(weeks=1),'%Y-%m-%d')
+defdate2 = datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d')
 
-
-logged = False
-print(logged)
 @app.route("/")
 def welcome():
-    return render_template("index.html", title="Welcome", loggedin=logged)
+    return render_template("index.html", title="Welcome")
 
 @app.route("/test1")
 def test1():
-    return render_template("test1.html", title="Testing1", loggedin=logged)
+    return render_template("test1.html", title="Testing1")
 
 @app.route("/test2")
 def test2():
-    return render_template("test2.html", title="Testing2", loggedin=logged)
+    return render_template("test2.html", title="Testing2", defdate1 = defdate1, defdate2=defdate2)
 
 @app.route("/getdata", methods=["POST"])
 def getdata():
@@ -25,25 +25,25 @@ def getdata():
     enddate = datetime.datetime.strptime(request.form['enddate'],'%Y-%m-%d')
     if startdate > enddate:
         startdate,enddate = enddate,startdate
+    print(startdate)
+    print(enddate)
     dfv.handle_data(startdate,enddate)
-    return render_template("test2.html", title="Testing2", loggedin=logged)
+    return render_template("test2.html", title="Testing2", defdate1 = startdate.strftime('%Y-%m-%d'),defdate2 = enddate.strftime('%Y-%m-%d'))
 
 @app.route("/form")
 def form():
-    return render_template("login.html", title="Login", loggedin=logged)
+    return render_template("login.html", title="Login")
 
 @app.route("/data")
 def data():
-    return render_template("data.html", title="Data", loggedin=logged)
+    return render_template("data.html", title="Data")
 
 @app.route("/login", methods=["POST"])
 def login():
     if request.form["username"]=="admin" and request.form["password"]=="admin":
-        logged = True
-        return render_template('loggedin.html', title="Successful login", loggedin=logged)
+        return render_template('loggedin.html', title="Successful login")
     else:
-        logged = False
-        return render_template('login.html', title="Failed login", loggedin=logged)
+        return render_template('login.html', title="Failed login")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
