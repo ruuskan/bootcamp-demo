8c02e869b3a983d70c7e020a670f2a05cd76d40b
Antti Ruuskanen <antti.ruuskanen@hotmail.com>
2021-10-29 11:30:41 +0300

    Added login and logout functionality
    	-logout.html added
    	-routing changed in app.py

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import datetime
 import requests
 from werkzeug.utils import redirect
 import data_from_veikkaus as dfv
+import os
+from dotenv import load_dotenv
+
 app = Flask(__name__)
-defdate1 = datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(weeks=1),'%Y-%m-%d')
-defdate2 = datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d')
+app.config['SECRET_KEY']='keepitsecretkeepitsafe'
+load_dotenv()
 
 @app.route("/")
 def welcome():
@@ -17,6 +20,8 @@ def test1():
 
 @app.route("/test2")
 def test2():
+    defdate1 = datetime.datetime.strftime(datetime.datetime.now()-datetime.timedelta(weeks=1),'%Y-%m-%d')
+    defdate2 = datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d')
     return render_template("test2.html", title="Testing2", defdate1 = defdate1, defdate2=defdate2)
 
 @app.route("/getdata", methods=["POST"])
@@ -28,20 +33,33 @@ def getdata():
     dfv.handle_data(startdate,enddate)
     return render_template("test2.html", title="Testing2", defdate1 = startdate.strftime('%Y-%m-%d'),defdate2 = enddate.strftime('%Y-%m-%d'))
 
-@app.route("/form")
-def form():
+@app.route("/login")
+def login():
     return render_template("login.html", title="Login")
 
 @app.route("/data")
 def data():
     return render_template("data.html", title="Data")
 
-@app.route("/login", methods=["POST"])
-def login():
-    if request.form["username"]=="admin" and request.form["password"]=="admin":
-        return render_template('loggedin.html', title="Successful login")
+@app.route("/loginsubmit", methods=["POST", 'GET'])
+def loginsubmit():
+    if request.method=='GET':
+        redirect('/login')
     else:
-        return render_template('login.html', title="Failed login")
+        if request.form['username']==os.environ['APP_USER'] and request.form["password"]==os.environ['APP_PASSWORD']:
+            session['loggedin'] = True
+            return render_template('logout.html', title="Successful login")
+        else:
+            return redirect('login')
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html", title="Log out")
+
+@app.route("/logoutsubmit", methods=["POST"])
+def logoutsubmit():
+    session.pop('loggedin',None)
+    return redirect('/login')
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
